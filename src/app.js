c08906e1589f489a4161c7663f6f65e56bd47ec6
Micael Souza <micael.souza@outlook.com>
2016-08-08 15:45:29 -0300

    Login route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import config from './app.config';
+import login from './features/login';
 import plano from './features/plano';
 
-angular.module('app', [uirouter, plano])
+angular.module('app', [uirouter, login, plano])
     .config(config);
---
+++ src/features/login/login.controller.js
@@ -0,0 +1,7 @@
+export default class LoginController {
+    constructor($scope) {
+        Object.assign(this, { $scope });
+    }
+}
+
+LoginController.$inject = ['$scope'];
