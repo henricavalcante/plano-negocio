1a51ee2e173f3dd2686f87400febbf505629fdf4
Henri Cavalcante <hnri_mxel@hotmail.com>
2017-01-19 17:41:30 -0300

    alteracao de texto proposta por aquiles

--- src/services/planoStatus.service.js
+++ src/services/planoStatus.service.js
@@ -90,7 +90,7 @@ class PlanoStatus {
           } else if (statusAtual == this.STATUSES.REVISADO && status == this.STATUSES.REVISANDO) {
             throw 'Não é posivel salvar alterações em um plano de negócios que já foi enviado para o aluno.';
           } else if (statusAtual == this.STATUSES.REVISADO && status == this.STATUSES.ENVIADO_REVISAO && bloqueio) {
-            throw 'Este plano já foi corrigido, não é possível enviar novamente para correção.';
+            throw 'Esse plano foi corrigido, recebido a revisão e enviado para avaliação final para crédito. Aguarde a confirmação do resultado da seleção para crédito.';
           } else {
             throw `Não é permitido salvar este plano de negócio com o status ${status} pois o status atual do plano é ${statusAtual}`;
           }
